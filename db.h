fb8a2247efaa3d7c9cfb600ce679ddac5223d9c6
Kristian Koskimäki <koskimaki@gmail.com>
2019-05-15 20:25:37 +0300

    Make member variables private and document functions

--- db.h
+++ db.h
@@ -13,20 +13,32 @@ public:
     Db(const QString &filename);
     ~Db() { _db.close(); _db = QSqlDatabase(); _db.removeDatabase(_connection); }
 
+private:
     QSqlDatabase _db;
     QString _connection;
     QString _id;
     QDateTime _modified;
 
 public:
+    //return md5 hash of parameter's file, or (as convinience) md5 hash of the file given to constructor
     QString uniqueId(const QString &filename="") const;
+
+    //constructor creates a database file if there is none already
     void createTables() const;
-    bool readMetadata(Video &video) const;        //return true if video is cached
+
+    //return true and updates member variables if the video metadata was cached
+    bool readMetadata(Video &video) const;
+
+    //save video properties in cache
     void writeMetadata(const Video &video) const;
+
+    //returns screen capture if it was cached, else return null ptr
     QByteArray readCapture(const int &percent) const;
+
+    //save image in cache
     void writeCapture(const int &percent, const QByteArray &image) const;
 
-    //removeVideo returns false if id not cached or could not be removed
+    //returns false if id not cached or could not be removed
     bool removeVideo(const QString &id) const;
 };
 
